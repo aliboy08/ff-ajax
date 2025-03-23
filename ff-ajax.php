a5713bdcb6be883000800cd798a289e9d38c6775
Alistair Ponce <alistair.ponce@gmail.com>
2025-03-24 08:51:45 +0900

    republish

--- ff-ajax.php
+++ ff-ajax.php
@@ -3,7 +3,7 @@
  * Plugin Name: FF Ajax
  * Plugin URI: https://www.fivebyfive.com.au/
  * Description: Simplify development involving ajax
- * Version: 2.3.5
+ * Version: 2.3.6
  * Author: Alistair Ponce
  * Author URI: https://github.com/aliboy08/
  * License: GPL v2 or later
--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import './main.scss';
 import FF_Ajax from './js/ff_ajax';
 
 document.addEventListener('DOMContentLoaded',()=>{
+    
     document.querySelectorAll('.ff_ajax').forEach(container=>{
         const settings = JSON.parse(container.dataset.settings);
         settings.container = container;
         container.ff_ajax = new FF_Ajax(settings);
-    })
-    document.dispatchEvent(new Event('ff_ajax_init')); 
+
+        const e = new Event('ff_ajax_init');
+        e.ff_ajax = container.ff_ajax;
+        document.dispatchEvent(e);
+    });
+    
 });
